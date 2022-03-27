afb1078cff4379c309bb72b37747de268ae6e2ae
hugosenari <hugosenari@gmail.com>
2022-03-27 18:25:51 -0300

    feat(nimpkgs): remove lock

--- master/flake.nix
+++ master/flake.nix
@@ -11,6 +11,7 @@
   inputs.src-parsegemini-master.ref   = "refs/heads/master";
   inputs.src-parsegemini-master.owner = "autumngray";
   inputs.src-parsegemini-master.repo  = "parsegemini";
+  inputs.src-parsegemini-master.dir   = "";
   inputs.src-parsegemini-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
